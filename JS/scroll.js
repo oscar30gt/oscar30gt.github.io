32e2ed96b058a239a7d67842783d25cf93a75221
Óscar Grimal Torres <oscar30gt@gmail.com>
2024-03-13 20:06:21 +0100

    .

--- JS/scroll.js
+++ JS/scroll.js
@@ -1,5 +1,19 @@
-
-
-document.addEventListener('scroll', function() {
-    
+document.addEventListener('DOMContentLoaded', () => {
+    applyHeroEffect();
 });
+
+document.addEventListener('scroll', () => {
+    applyHeroEffect();
+});
+
+function applyHeroEffect()
+{
+    const effects = document.querySelectorAll("#Background_effects div div");
+    const currentScroll = window.scrollY;
+
+    effects.forEach(effect => 
+    {
+        var multiplier = effect.offsetHeight / 80;
+        effect.style.bottom = `${(currentScroll + 200) * multiplier - 100}px`;
+    });
+}
