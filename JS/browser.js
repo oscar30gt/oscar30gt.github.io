93fee5dd41fe5079add055d94d77be4d99fdabb3
Óscar Grimal Torres <oscar30gt@gmail.com>
2024-03-14 21:20:31 +0100

    a

--- JS/browser.js
+++ JS/browser.js
@@ -2,8 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
     var isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
     var scrollBox = document.querySelector(".horizontal_scroll > div > div")
 
-    if (isSafari)
-    {
+ 
         scrollBox.style.padding = "0 50px 0 0";
-    }
+    
 })
