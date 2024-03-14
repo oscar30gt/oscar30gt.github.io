a6743f67c92b2bca60ed61f7f64325aff3f6adda
Óscar Grimal Torres <oscar30gt@gmail.com>
2024-03-14 21:50:54 +0100

    Update browser.js

--- JS/browser.js
+++ JS/browser.js
@@ -1,5 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
-    var isSafari = user.browser.family === 'Safari'
+
+    // Checks the safari's scroll padding issue
+    var isSafari = /constructor/i.test(window.HTMLElement) 
+        || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] 
+        || (typeof safari !== 'undefined' && window['safari'].pushNotification));
+        
     if (isSafari)
     {   
         var scrollBox = document.querySelector(".horizontal_scroll > div > div")
