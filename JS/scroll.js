183b3ad4f8732033f6c4147c06aba3d10aa7b081
Óscar Grimal Torres <oscar30gt@gmail.com>
2024-03-15 18:58:49 +0100

    Update scroll.js

--- JS/scroll.js
+++ JS/scroll.js
@@ -11,12 +11,14 @@ function applyHeroEffect()
     const effects = document.querySelectorAll("#Background_effects div div");
     const currentScroll = window.scrollY;
 
-    effects.forEach(effect => 
+    for (let i = 0; i < effects.length; i++) 
     {
+        const effect = effects[i];
         var multiplier = effect.offsetHeight / 80;
         effect.style.bottom = `${(currentScroll + 200) * multiplier - 100}px`;
-    });
+    }
 }
+
 
 function scrollNewsBy(dir)
 {
